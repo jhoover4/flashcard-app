e7d75ffef34eca860a1dc855d262ac4076714dbf
jhoover4 <jordan@hoovermld.com>
2018-10-04 17:51:46 -0500

    Updated html and css with hardcoded data.

--- static/js/vue.js
+++ static/js/vue.js
@@ -3,20 +3,28 @@ var app = new Vue({
     delimiters: ['${', '}'],
     data: {
         appTitle: "Javascript Card App",
-        cardCount: 0,
-        cards: [],
-        currentCard: 0,
+        cardCount: 1,
+        cards: [
+            {
+                title: "Test",
+                question: "Test question content",
+                answer: "Test answer content",
+            }
+        ],
         correct: 0,
         wrong: 0
     },
     methods: {
         increaseScore: function () {
             this.correct++;
-            this.currentCard++;
+            this.cardCount++;
         },
         decreaseScore: function () {
             this.wrong++;
-            this.currentCard++;
+            this.cardCount++;
+        },
+        skipCard: function () {
+            this.cardCount++;
         }
     }
 });
