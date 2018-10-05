57e6575e9395b20910b09d522b2299050a5c7b5d
jhoover4 <jordan@hoovermld.com>
2018-10-05 12:38:21 -0500

    Added form for card creation and new styling to front end.

--- static/js/vue.js
+++ static/js/vue.js
@@ -1,31 +1,80 @@
 var app = new Vue({
-    el: '#root',
-    delimiters: ['${', '}'],
-    data: {
-        appTitle: "Javascript Card App",
-        cardCount: 1,
-        cards: [
-            {
-                title: "Test",
-                question: "Test question content",
-                answer: "Test answer content",
+        el: '#root',
+        delimiters: ['${', '}'],
+        data: {
+            cardCount: 1,
+            cards: [],
+            subjects: [],
+            correct: 0,
+            wrong: 0,
+            cardFlipped: false,
+            addCardError: false,
+            formFieldAnswer: "",
+            formFieldQuestion: "",
+            formFieldSubject: ""
+        },
+        methods: {
+            increaseScore: function () {
+                this.correct++;
+                this.cardCount++;
+                this.cardFlipped = false
+            },
+            decreaseScore: function () {
+                this.wrong++;
+                this.cardCount++;
+                this.cardFlipped = false
+            },
+            skipCard: function () {
+                this.cardCount++;
+                this.cardFlipped = false
+            },
+            toggleCard: function () {
+                this.cardFlipped = !this.cardFlipped
+            },
+            createCard: function (e) {
+                e.preventDefault();
+                if (this.formFieldAnswer === "" || this.formFieldQuestion === "" || this.formFieldSubject === "") {
+                    this.addCardError = true;
+                } else {
+                    let newCard =
+                        {
+                            question: this.formFieldQuestion,
+                            answer: this.formFieldAnswer,
+                            subject: this.formFieldSubject
+                        };
+
+                    this.cards.push(newCard);
+
+                    this.postNewCard(newCard);
+                }
+            },
+            postNewCard: function (cardData) {
+                let csrftoken = Cookies.get('csrftoken');
+
+                fetch('/api/cards/',
+                    {
+                        method: 'POST',
+                        body: JSON.stringify(cardData),
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'X-CSRFToken': csrftoken,
+                        },
+                        credentials: 'same-origin',
+                    })
+                    .catch(error => console.log(error));
             }
-        ],
-        correct: 0,
-        wrong: 0
-    },
-    methods: {
-        increaseScore: function () {
-            this.correct++;
-            this.cardCount++;
         },
-        decreaseScore: function () {
-            this.wrong++;
-            this.cardCount++;
-        },
-        skipCard: function () {
-            this.cardCount++;
+        mounted() {
+            fetch('/api/cards/')
+                .then(response => response.json())
+                .then(jsonData => (this.cards = jsonData))
+                .catch(error => console.log(error));
+
+            fetch('/api/subjects/')
+                .then(response => response.json())
+                .then(jsonData => (this.subjects = jsonData.map(subject => subject.name)))
+                .catch(error => console.log(error));
         }
-    }
-});
+    })
+;
 
